7db841b775821818024cddcc9b4665d9c9aafc37
unknown <ankit ramola>
2020-04-08 00:33:18 +0530

    new changes

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url ='https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYWFuY2hhbDA4MDgiLCJhIjoiY2s4ZzQ0MmZ3MDd2bjNncGJseHVxaTUyMCJ9.uJCEFBCw0rYkj9ja_BsUHw&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
